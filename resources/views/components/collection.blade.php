f992dc5e7db5bc78eeb6b5c31bf45b3963c84a60
SKAntoniou <SKAntoniou@gmail.com>
2025-07-07 15:09:19 +0100

    (Refactor): Spacing on homepage
    Made everything use the same tailwind classes for the most parent container.

--- resources/views/components/collection.blade.php
+++ resources/views/components/collection.blade.php
@@ -1,5 +1,3 @@
-@
-
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-[1fr_2fr]">
 
   <div class="">
@@ -8,11 +6,7 @@
 
   <div class="">
 
-    @foreach ($product in $collection)
-
-
-
-    @endforeach
+    
 
   </div>
 
